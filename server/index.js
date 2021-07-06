9950e1e9ef85635857c082aaf6e0cf596339d4b6
Nicholas Pym <nicholasp@bbd.co.za>
2021-07-06 21:04:09 +0200

    Added logger and updated express setup

--- server/index.js
+++ server/index.js
@@ -1,8 +1,38 @@
-const express = require('express');
+const express = require("express");
 const app = express();
- 
-app.get('/', function (req, res) {
-  res.send('Hello World');
-})
- 
-app.listen(3000);
+const helmet = require("helmet");
+const cors = require("cors");
+const path = require("path");
+const root = require("./util/root");
+const logger = require("./util/winston");
+const port = 8080;
+
+app.use(helmet());
+
+app.use(
+  cors({
+    origin: "",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.use((error, req, res, next) => {
+  logger.log({
+    logger: "error",
+    message: error.stack,
+  });
+  const status = error.statusCode || 500;
+  const message = error.message.replace(/^\[.*\](\t){1,}/g, "");
+  const data = error.data;
+  res.status(status).json({ message: message, data: data, success: false });
+});
+
+const server = app.listen(port, () => {
+  logger.log({
+    logger: "info",
+    message: `[Index.js]\tServer listening at http://localhost:${port}.`,
+  });
+});
+
+module.exports = app;
