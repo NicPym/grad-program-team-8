5be0519a750fc3dc07e82a08d54164caf3fdc031
Shaikjee, Mohammed, Vodacom South Africa (External) <Mohammed.Shaikjee@vcontractor.co.za>
2021-07-10 18:14:08 +0200

    get all posts in db

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -2,4 +2,24 @@ const posts = require("express").Router();
 const models = require("../models").sequelize.models;
 const { dataCleaner, formatDate } = require("../util/helpers");
 
+posts.get("/", (req, res, next) => {
+
+  models.Post.findAll({
+  })
+    .then((posts) => {
+      const { rows } = dataCleaner(posts);
+
+      res.json(
+        rows.map((row) => {
+          return {
+            id: row.pkPost,
+            text: row.cText,
+            createdAt: row.createdAt,
+          };
+        })
+      );
+    })
+    .catch((err) => next(err));
+});
+
 module.exports = posts;
