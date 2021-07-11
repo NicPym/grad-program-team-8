913ebb6f78cd2976d486f4816c609abf457658e8
TafLaw <tmuzeren@student.wethinkcode.co.za>
2021-07-11 17:06:06 +0200

    post index

--- server/routes/ui.js
+++ server/routes/ui.js
@@ -2,11 +2,7 @@ const path = require("path");
 const ui = require("express").Router();
 const authenticate = require("../util/authenticate");
 
-<<<<<<< HEAD
-const appDir = path.resolve(__dirname, '../../') + '/app/views';
-=======
 const appDir = path.resolve(__dirname, "../../") + "/app/";
->>>>>>> ac8ee25a559241d32b75672fe77ebcd761a53338
 
 ui.get("/", function( req, res ) {
   res.sendFile(path.join(appDir, "index.html"));
